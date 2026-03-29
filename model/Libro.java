86097936cb1258b344cd9f968759adea0f83259c
Bruno Sebastian Sandoval <brunosandovalsebastian@gmail.com>
2026-03-28 21:00:47 -0300

    Estructura inicial del TP de Biblioteca

---
+++ model/Libro.java
@@ -0,0 +1,62 @@
+package model;
+
+public class Libro {
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private int anio;
+    private boolean disponible;
+
+    public Libro(String isbn, String titulo, String autor, int anio)
+    {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anio = anio;
+        this.disponible = true;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    @Override
+    public String toString() {
+        return "Libro [ISBN=" + isbn + ", Título=" + titulo + ", Autor=" + autor + ", Disponible=" + disponible + "]";
+    }
+}
